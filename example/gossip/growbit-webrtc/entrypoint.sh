66e944df3b87e00c83e3fc3b8d15975d7fbc8d3b
Alessandro Aglietti <alessandro.aglietti@brumbrum.it>
2019-03-01 14:52:06 +0100

    from 1.21GB container to 324MB

--- example/gossip/growbit-webrtc/entrypoint.sh
+++ example/gossip/growbit-webrtc/entrypoint.sh
@@ -11,9 +11,13 @@ zip oracle.zip *
 
 curl --silent -o /opt/oracle/infura.json "https://ipfs.infura.io:5001/api/v0/add?pin=false" -X POST -H "Content-Type: multipart/form-data" -F file=@"/opt/oracle/oracle.zip"
 
+rm -f oracle.zip
+
 jq "." /opt/oracle/infura.json
 
 ORACLE_DAG=`jq -r ".Hash" /opt/oracle/infura.json`
+
+rm -f infura.json
 
 cd /usr/src/app
 
