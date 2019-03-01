0711cef69e98598b439ffe9a066815e47e05b5ff
Alessandro Aglietti <alessandro.aglietti@brumbrum.it>
2019-03-01 13:45:01 +0100

    using infura instead ipdfs daemon to publish the oracle on IPFS

--- example/gossip/growbit-webrtc/entrypoint.sh
+++ example/gossip/growbit-webrtc/entrypoint.sh
@@ -3,34 +3,17 @@
 set -x -e
 
 apt-get update
-apt-get install -y zip
-
-cd /opt
-
-wget -O go-ipfs.tar.gz "https://dist.ipfs.io/go-ipfs/v0.4.18/go-ipfs_v0.4.18_linux-amd64.tar.gz"
-
-tar xvfz go-ipfs.tar.gz
-
-cd go-ipfs
-
-./install.sh
-
-ipfs init
-
-ipfs daemon &
-
-# seems the http gateway start before the complete bootstrap
-#curl --output /dev/null --silent --retry 5 --retry-connrefused --head --fail http://localhost:8080/ipfs/QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG/readme
-
-echo "wait 10s for ipfs daemon bootstrap"
-
-sleep 10
+apt-get install -y zip jq
 
 cd /opt/oracle
 
 zip oracle.zip *
 
-ORACLE_DAG=`ipfs add -Q oracle.zip`
+curl --silent -o /opt/oracle/infura.json "https://ipfs.infura.io:5001/api/v0/add?pin=false" -X POST -H "Content-Type: multipart/form-data" -F file=@"/opt/oracle/oracle.zip"
+
+jq "." /opt/oracle/infura.json
+
+ORACLE_DAG=`jq -r ".Hash" /opt/oracle/infura.json`
 
 cd /usr/src/app
 
